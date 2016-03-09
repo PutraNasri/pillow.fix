8d8e95328a2cf27d33a55ee0a53137a5ef981ad9
PutraNasri <nasrisyawaldi@gmail.com>
2016-03-09 17:33:49 +0700

    9 maret 16

--- app/src/main/java/es/esy/kinketkuena/pillowfix/isiform/form.java
+++ app/src/main/java/es/esy/kinketkuena/pillowfix/isiform/form.java
@@ -1,20 +1,14 @@
 package es.esy.kinketkuena.pillowfix.isiform;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Base64;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,37 +17,23 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.model.LatLng;
-
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import es.esy.kinketkuena.pillowfix.R;
 import es.esy.kinketkuena.pillowfix.RequestHandler;
 import es.esy.kinketkuena.pillowfix.config;
 
-public class form extends AppCompatActivity {
+public class form extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private Location mLastLocation;
     public LocationManager mLocationManager;
@@ -69,17 +49,16 @@ public class form extends AppCompatActivity {
     private Spinner spinnerdaerah, spinnerper;
     public static final int REQUEST_CAPTURE = 1;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-
         Button clik = (Button) findViewById(R.id.cokfoto);
         if (!hasCamera()) {
             clik.setEnabled(false);
         }
-
-
         editTextnamakost = (EditText) findViewById(R.id.editTextnamakost);
         editTextnamapemilik = (EditText) findViewById(R.id.editTextnamapemilik);
         editTextnohp = (EditText) findViewById(R.id.editTextnohp);
@@ -101,22 +80,17 @@ public class form extends AppCompatActivity {
         item.add("Jaya baru");
         item.add("Kuta alam");
         spinnerper = (Spinner) findViewById(R.id.spinnerper);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(form.this,android.R.layout.simple_spinner_dropdown_item,item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(form.this, android.R.layout.simple_spinner_dropdown_item, item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerdaerah.setAdapter(adapter);
-
     }
     //Adding an employee
     private void addEmployee() {
-
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Bitmap bitmap = ((BitmapDrawable)imageViewfoto.getDrawable()).getBitmap();
+        Bitmap bitmap = ((BitmapDrawable) imageViewfoto.getDrawable()).getBitmap();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
         byte[] fotoo = baos.toByteArray();
-
-
         //proses pengambilan string dari variabel
-
         final String namakost = editTextnamakost.getText().toString().trim();
         final String namapemilik = editTextnamapemilik.getText().toString().trim();
         final String nohp = editTextnohp.getText().toString().trim();
@@ -129,59 +103,49 @@ public class form extends AppCompatActivity {
         final String lokasi = editTextlokasi.getText().toString().trim();
         final String foto = Base64.encodeToString(fotoo, Base64.DEFAULT);
 
-
-        class AddEmployee extends AsyncTask<Void,Void,String>{
+        class AddEmployee extends AsyncTask<Void, Void, String> {
 
             ProgressDialog loading;
-
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                loading = ProgressDialog.show(form.this,"Mengirim...","Wait...",false,false);
+                loading = ProgressDialog.show(form.this, "Mengirim...", "Wait...", false, false);
             }
-
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 loading.dismiss();
-                Toast.makeText(form.this,s,Toast.LENGTH_LONG).show();
+                Toast.makeText(form.this, s, Toast.LENGTH_LONG).show();
             }
-
             @Override
             protected String doInBackground(Void... v) {
-                HashMap<String,String> params = new HashMap<>();
-                params.put(config.KEY_EMP_NAME,namakost);
-                params.put(config.KEY_EMP_DESG,namapemilik);
-                params.put(config.KEY_EMP_SAL,nohp);
-                params.put(config.KEY_EMP_NIK,nik);
-                params.put(config.KEY_EMP_ALAMAT,alamat);
-                params.put(config.KEY_EMP_DAERAH,daerah);
-                params.put(config.KEY_EMP_HARGA,harga);
-                params.put(config.KEY_EMP_JANGKA,jangka);
-                params.put(config.KEY_EMP_DESKRIPSI,deskripsi);
-                params.put(config.KEY_EMP_LOKASI,lokasi);
-                params.put(config.KEY_EMP_FOTO,foto);
+                HashMap<String, String> params = new HashMap<>();
+                params.put(config.KEY_EMP_NAME, namakost);
+                params.put(config.KEY_EMP_DESG, namapemilik);
+                params.put(config.KEY_EMP_SAL, nohp);
+                params.put(config.KEY_EMP_NIK, nik);
+                params.put(config.KEY_EMP_ALAMAT, alamat);
+                params.put(config.KEY_EMP_DAERAH, daerah);
+                params.put(config.KEY_EMP_HARGA, harga);
+                params.put(config.KEY_EMP_JANGKA, jangka);
+                params.put(config.KEY_EMP_DESKRIPSI, deskripsi);
+                params.put(config.KEY_EMP_LOKASI, lokasi);
+                params.put(config.KEY_EMP_FOTO, foto);
 
                 RequestHandler rh = new RequestHandler();
                 String res = rh.sendPostRequest(config.URL_ADD, params);
                 return res;
             }
         }
-
         AddEmployee ae = new AddEmployee();
         ae.execute();
     }
-
     public void onClick(View v) {
-
         //pengeceka nanti di buat disini
-
-
         addEmployee();
-
     }
     public boolean hasCamera() {
-        return  getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
     public void cokfotoile(View v) {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -195,6 +159,65 @@ public class form extends AppCompatActivity {
             Bitmap photo = (Bitmap) extras.get("data");
             imageViewfoto.setImageBitmap(photo);
         }
+    }
+    public void coklokasi(View v) {
+//proses pengambilan lokasi gps
+        int LOCATION_REFRESH_TIME = 0;
+        int LOCATION_REFRESH_DISTANCE = 100;
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
+    }
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            System.out.println("onLocationChanged");
+            mLastLocation = location;
+            editTextlokasi.setText("Latitude:" + String.valueOf(location.getLatitude()) + "," + "Longitude:" + String.valueOf(location.getLongitude()));
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            System.out.println("onStatusChanged");
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+            System.out.println("onProviderEnabled");
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+            System.out.println("onProviderDisabled");
+        }
+    };
+    //////////////////////////////////////////////////////////////////
+    @Override
+    public void onConnected(Bundle bundle) {
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
     }
 
 }
